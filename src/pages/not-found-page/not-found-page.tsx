8e9857c284a2ee22441a6baa60f60ed48401655b
Katerina <kathty@gmail.com>
2022-05-20 03:45:28 +0300

    Добавляет компонент NotFoundPage

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function NotFoundPage(): JSX.Element {
+  return (
+    <section>
+      <h1>404. Page not found</h1>
+      <Link to={AppRoute.Main}>Вернуться на главную</Link>
+    </section>
+  );
+}
+
+export default NotFoundPage;
